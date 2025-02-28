b20cfbf9ba917502ec3a416d4e2074f534767672
dengfei <87270678+mou-fly@users.noreply.github.com>
2025-03-01 00:25:46 +0800

    Add files via upload

--- by_hand/transformer/multi-head_attention.py
+++ by_hand/transformer/multi-head_attention.py
@@ -1,46 +1,45 @@
 import torch
-import torch.functional as F
-from torch import nn
+import torch.nn as nn
 import math
 
-test = torch.randn(128, 64, 512)
-
-d_model = 512
 n_head = 8
+d_model = 512
+x = torch.randn(128, 64, d_model)
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super(TokenEmbedding, self).__init__(vocab_size, d_model)
+
+class PositionEmbedding(nn.Module):
+    def __init__(self, d_model):
 
 
-class multi_haed_attention(nn.Module):
+class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, n_head):
-        super(multi_haed_attention, self).__init__()
-
+        super(MultiHeadAttention, self).__init__()
         self.d_model = d_model
         self.n_head = n_head
-        self.w_q = nn.Linear(d_model, d_model)
-        self.w_k = nn.Linear(d_model, d_model)
-        self.w_v = nn.Linear(d_model, d_model)
-        self.w_combine = nn.Linear(d_model, d_model)
+        self.d_h = self.d_model // self.n_head
+        self.w_q = nn.Linear(d_model, self.d_h * n_head)
+        self.w_k = nn.Linear(d_model, self.d_h * n_head)
+        self.w_v = nn.Linear(d_model, self.d_h * n_head)
         self.softmax = nn.Softmax(dim=-1)
+        self.concat = nn.Linear(d_model, self.d_h * n_head)
 
     def forward(self, q, k, v, mask=None):
-        batch_size, time, dimension = q.shape
-        n_d = self.d_model / self.n_head
-        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        batch, seq_len, _ = q.size()
+        q = self.w_q(q).view(batch, seq_len, self.n_head, self.d_h).permute(0, 2, 1, 3)
+        k = self.w_k(k).view(batch, seq_len, self.n_head, self.d_h).permute(0, 2, 1, 3)
+        v = self.w_v(v).view(batch, seq_len, self.n_head, self.d_h).permute(0, 2, 1, 3)
 
-        q = q.view(batch_size, time, self.n_head, n_d).permute(0, 2, 1, 3)
-        k = k.view(batch_size, time, self.n_head, n_d).permute(0, 2, 1, 3)
-        v = v.view(batch_size, time, self.n_head, n_d).permute(0, 2, 1, 3)
-
-        score = q @ k.transpose(2, 3) / math.sqrt(n_d)
-        mask = torch.tril(torch.ones_like(time, time, dtype=bool), diagonal=0)
-        score = score.masked_fill(mask, -math.inf)
+        score = q @ k.transpose(2, 3) / math.sqrt(self.d_h)
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -1e9)
         score = self.softmax(score) @ v
-
-        score = score.permute(0, 2, 1, 3).contiguous().view(batch_size, time, dimension)
-
-        output = self.w_combine(score)
+        score = score.permute(0, 2, 1, 3).contiguous().view(batch, seq_len, d_model)
+        output = self.concat(score)
         return output
 
 
-attention = multi_haed_attention(d_model, n_head)
-output = attention(test, test, test)
-print(output)
+model = MultiHeadAttention(d_model, n_head)
+print(model(x, x, x))
